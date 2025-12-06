96fda04be8528f245450ff97e0d980e30fd04576
Ricardo Serta <ricardo.serta2@gmail.com>
2025-12-06 11:24:42 -0300

    Configura WhiteNoise para servir arquivos estáticos

--- core/settings.py
+++ core/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,11 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-from pathlib import Path
-BASE_DIR = Path(__file__).resolve().parent.parent  # isso já deve existir
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
@@ -141,6 +141,12 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'meu_perfil'
 LOGOUT_REDIRECT_URL = 'login'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+}
 
